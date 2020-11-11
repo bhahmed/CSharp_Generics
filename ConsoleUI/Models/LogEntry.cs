df1b7bc59f32897c57ca67ff9084ec761f5b77c4
AhmedBenHmida <ahmed.benhmida@newaccess.ch>
2020-11-11 23:10:43 +0100

    Add project files.

---
+++ ConsoleUI/Models/LogEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI.Models
+{
+    public class LogEntry
+    {
+        public string Message { get; set; }
+
+        public int ErrorCode { get; set; }
+
+        public DateTime TimeOfEvent { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ ConsoleUI/WithoutGenerics/OriginalTextFileProcessor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleUI.Models;
+
+namespace ConsoleUI.WithoutGenerics
+{
+    class OriginalTextFileProcessor
+    {
+        public static void SavePeople(List<Person> people, string file)
+        {
+            List<string> lines = new List<string>();
+
+            // Add a header row
+            lines.Add("FirstName,IsAlive,LastName");
+
+            foreach (var p in people)
+            {
+                lines.Add($"{ p.FirstName },{ p.IsAlive },{ p.LastName }");
+            }
+
+            System.IO.File.WriteAllLines(file, lines);
+        }
+
+        public static List<Person> LoadPeople(string file)
+        {
+            List<Person> output = new List<Person>();
+            Person p;
+            var lines = System.IO.File.ReadAllLines(file).ToList();
+
+            // Remove the header
+            lines.RemoveAt(0);
+
+            foreach (var line in lines)
+            {
+                var vals = line.Split(',');
+                p = new Person
+                {
+                    FirstName = vals[0],
+                    IsAlive = bool.Parse(vals[1]),
+                    LastName = vals[2]
+                };
+
+                output.Add(p);
+            }
+
+            return output;
+        }
+    }
+}
